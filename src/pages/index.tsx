1b5f28b5372ca7cbd1278996b036f9518ed9a0b2
yhwa <yhwabillie93@gmail.com>
2024-07-18 21:55:32 +0900

    feat: 사용하지 않는 링크 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,11 +16,7 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+        <div className={styles.buttons}></div>
       </div>
     </header>
   )
